147fb4985c9fe7447c9251df6ec6ecca41a9c982
Thiago Vieira <devthiagovieira@gmail.com>
2025-02-14 20:14:17 -0300

    Operador ternário

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,25 +1,44 @@
 public class Operadores {
   public static void main(String[] args) {
 
+    // Operador ternário
+    int a, b;
+    a = 5;
+    b = 6;
+
+    String resultado = "";
+
+    resultado = a == b ? "Verdadeiro" : "Falso";
+    System.out.println(resultado);
+
+    if (a == b) {
+      resultado = "Verdadeiro";
+    } else {
+      resultado = "Falso";
+    }
+    System.out.println(resultado);
+
+    ///////////////////////////////////////
+
     // Operador de negação
-    boolean variavel = true;
-    System.out.println(variavel);
-    variavel = !variavel;
-    System.out.println(variavel);
-    System.out.println(!variavel);
+    // boolean variavel = true;
+    // System.out.println(variavel);
+    // variavel = !variavel;
+    // System.out.println(variavel);
+    // System.out.println(!variavel);
 
-    // Operadores de incremento
-    int numero = 5;
-    numero = numero + 2;
-    System.out.println(numero);
+    // // Operadores de incremento
+    // int numero = 5;
+    // numero = numero + 2;
+    // System.out.println(numero);
 
-    System.out.println(numero++);
-    System.out.println(numero);
-    System.out.println(++numero);
+    // System.out.println(numero++);
+    // System.out.println(numero);
+    // System.out.println(++numero);
 
-    System.out.println(numero--);
-    System.out.println(numero);
-    System.out.println(--numero);
+    // System.out.println(numero--);
+    // System.out.println(numero);
+    // System.out.println(--numero);
 
     /////////////////////////////////////////////
 
