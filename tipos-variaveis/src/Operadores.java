548edab8f1f8d8d9b6fa7b62a78a9e63eee48635
Thiago Vieira <devthiagovieira@gmail.com>
2025-02-14 19:53:17 -0300

    Operadores Unarios

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,20 +1,30 @@
 public class Operadores {
   public static void main(String[] args) {
-    String concatenacao = "?";
+    // int numero = 5;
 
-    concatenacao = 1 + 1 + 1 + "1";
-    System.out.println(concatenacao);
+    // System.out.println(-numero);
+    // System.out.println(numero);
 
-    concatenacao = 1 + "1" + 1 + 1;
-    System.out.println(concatenacao);
+    // numero = -numero;
+    // System.out.println(numero);
 
-    concatenacao = 1 + "1" + 1 + "1";
-    System.out.println(concatenacao);
+    // numero = numero * -1;
+    // System.out.println(numero);
 
-    concatenacao = "1" + 1 + 1 + 1;
-    System.out.println(concatenacao);
+    // String concatenacao = "?";
+    // concatenacao = 1 + 1 + 1 + "1";
+    // System.out.println(concatenacao);
 
-    concatenacao = "1" + (1 + 1 + 1);
-    System.out.println(concatenacao);
+    // concatenacao = 1 + "1" + 1 + 1;
+    // System.out.println(concatenacao);
+
+    // concatenacao = 1 + "1" + 1 + "1";
+    // System.out.println(concatenacao);
+
+    // concatenacao = "1" + 1 + 1 + 1;
+    // System.out.println(concatenacao);
+
+    // concatenacao = "1" + (1 + 1 + 1);
+    // System.out.println(concatenacao);
   }
 }
