bd90e38ee455af8cdd75d72b15148cc59c377591
Thiago Vieira <devthiagovieira@gmail.com>
2025-02-14 19:31:46 -0300

    Conteudo Tipos de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    // Estude Tipos Primitivos
+    // Estude Classe String
+
+    // double salarioMinimo = 2500;
+    // short numeroCurto = 1;
+    // int numeroNormal1 = numeroCurto;
+    // short numeroCurto2 = (short) numeroNormal1;
+
+    // Variável
+    int numero = 5;
+    numero = 10;
+    System.out.println(numero);
+
+    // Constante
+    final double VALOR_DE_PI = 3.1415;
+    // VALOR_DE_PI = 10.15;
+    System.out.println(VALOR_DE_PI);
   }
 }
