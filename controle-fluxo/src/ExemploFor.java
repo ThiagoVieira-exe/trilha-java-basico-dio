aa10c3f7b134e9e096688aaee03faa8e46766362
Thiago Vieira <devthiagovieira@gmail.com>
2025-02-21 20:28:17 -0300

    Controle de floxo - For

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+
+  public static void main(String[] args) {
+    for (int carneinhos = 1; carneinhos <= 20; carneinhos++) {
+      System.out.println("Contando carneirinhos: " + carneinhos);
+    }
+    System.out.println("Joãozinho dormiu.");
+  }
+}
